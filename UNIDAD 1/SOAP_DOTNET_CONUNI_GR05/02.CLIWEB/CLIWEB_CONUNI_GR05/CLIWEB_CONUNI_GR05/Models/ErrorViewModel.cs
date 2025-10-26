50b72265549c0f6b5b4734e41d4ed8909bb936e3
Jcnazate11 <jcnazate@espe.edu.ec>
2025-10-26 10:45:00 -0500

    cliente web java restfull

--- UNIDAD 1/SOAP_DOTNET_CONUNI_GR05/02.CLIWEB/CLIWEB_CONUNI_GR05/CLIWEB_CONUNI_GR05/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CLIWEB_CONUNI_GR05.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
